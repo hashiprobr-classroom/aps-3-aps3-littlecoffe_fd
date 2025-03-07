5807d41b8a94dcc78ff7beb6a8ec3a8685b61992
Denis Alonso <denisalonso1999@gmail.com>
2025-03-06 23:59:50 -0300

    last

--- unter/src/main/java/br/edu/insper/desagil/aps3/unter/Motorista.java
+++ unter/src/main/java/br/edu/insper/desagil/aps3/unter/Motorista.java
@@ -1,7 +1,9 @@
 package br.edu.insper.desagil.aps3.unter;
 
 public class Motorista extends Usuario{
-
+    public Motorista(String cpf, String nome){
+        super(cpf, nome);
+    }
 
     @Override
     public avalia(Corrida corrida, int nota){
--- unter/src/test/java/br/edu/insper/desagil/aps3/unter/CentralTest.java
+++ unter/src/test/java/br/edu/insper/desagil/aps3/unter/CentralTest.java
@@ -48,4 +48,28 @@ public class CentralTest {
 
         assertEquals(4.5,c.mediaPassageiros("12345678900"));
     }
+    @Test
+    void avaliacoesMotorista() {
+        m = new Motorista("12345678900", "Rafael");
+        m1 = new Motorista("23456789011", "Stefane");
+        co = new Corrida(p);
+        co1 = new Corrida(p);
+        co2 = new Corrida(p1);
+        co3 = new Corrida(p1);
+        co4 = new Corrida(p1);
+        co.setMotorista(m);
+        co1.setMotorista(m);
+        co2.setMotorista(m1);
+        co3.setMotorista(m1);
+        co.avaliaMotorista(2);
+        c.adiciona(co);
+        co1.avaliaMotorista(3);
+        c.adiciona(co1);
+        co2.avaliaMotorista(1);
+        c.adiciona(co2);
+        co3.avaliaMotorista(1);
+        c.adiciona(co3);
+
+        assertEquals(4.5,c.mediaPassageiros("12345678900"));
+    }
 }
